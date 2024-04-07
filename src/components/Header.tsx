5b68f34bf46d576a1866dd953dc2b32a6cd2ed77
mateusfranca97 <mateus10gomes@gmail.com>
2024-04-07 06:07:03 -0300

    fix: glow, hover color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { Download } from "lucide-react"
 import { Button } from "./ui/button"
 
 const Header = () => {
@@ -6,7 +7,7 @@ const Header = () => {
             <h1 className="font-medium w-80 pointer-events-none select-none">Mateus <span className="text-gray-400">, FullStack</span></h1>
             <div className="hidden lg:flex">
                 <div className="flex gap-4">
-                    <Button variant="outline" className="rounded-full">Baixar Curriculo</Button>
+                    <Button variant="outline" className="rounded-full gap-2 select-none">Baixar Curriculo <Download width={15}/></Button>
                 </div>
             </div>
         </header>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,38 +1,33 @@
 import { GlowCapture } from "@codaworks/react-glow"
 import { Button } from "./ui/button"
 import Card from "./Card"
-import CardEffect from "./CardEffect"
+import { Forward } from "lucide-react"
 
 const Hero = () => {
   return (
 
-    <main className='h-[33rem] flex flex-col lg:grid grid-cols-12 gap-2'>
+    <GlowCapture className='h-[33rem] flex flex-col lg:grid grid-cols-12 gap-2'>
       <div className="h-full col-span-8">
         <div className="h-96 text-white text-2xl flex flex-col justify-end gap-10">
-          <h1 className="lg:text-5xl font-bold leading-snug">Vamos COMEÇAR <br />a construir algo grande <br /> juntos ?</h1>
-          <Button className="w-40 rounded-full">Fale comigo</Button>
+          <h1 className="lg:text-5xl font-bold leading-snug select-none">Vamos <b className="text-tomato-900">COMEÇAR</b><br /> a construir algo grande <br /> juntos ?</h1>
+          <Button className="w-40 rounded-full hover:bg-tomato-900 hover:text-black transition-all duration-300 gap-2">Fale comigo <Forward width={15}/></Button>
         </div>
       </div>
-      <div className="pt-10 lg:h-full lg:pt-0 col-span-4 rounded-2xl text-white">
-        <GlowCapture>
+      <div className="pt-10 lg:h-full lg:pt-0 col-span-4 text-white">
           <div className="grid grid-cols-2 gap-3">
             <Card >Sobre mim</Card>
             <Card>Meus Projetos</Card>
           </div>
-        </GlowCapture>
-        <GlowCapture>
           <div className="grid grid-cols gap-3 mt-3">
             <Card width="large">Experiencias</Card>
           </div>
-        </GlowCapture>
-        <GlowCapture>
           <div className="grid grid-cols-2 gap-3 pt-4">
             <Card>Midia </Card>
             <Card height="large"></Card>
           </div>
-        </GlowCapture>
       </div>
-    </main>
+      <div className="w-36 h-36 bg-tomato-900/30 absolute blur-3xl top-[10rem]"></div>
+    </GlowCapture>
   )
 }
 
